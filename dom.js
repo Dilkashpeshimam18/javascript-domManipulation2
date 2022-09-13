ad739759531e2d986bd500b220aa27f5724d1a7f
Dilkash <dilkashpeshimam@gmail.com>
2022-09-13 17:17:52 +0530

    second commit

--- dom.js
+++ dom.js
@@ -7,12 +7,16 @@ console.log(document.all[10])
 document.all[10].textContent='Item Cart'
 
 console.log(document.forms)
+
+//Get element by id
 let headerTitle= document.getElementById("header-title")
 let headerBottom=document.getElementById('main-header')
 headerTitle.textContent='Hello'
 headerTitle.innerText='Cart'
 
 headerBottom.style.borderBottom='solid 3px #000'
+
+//Get element by classname
 
 let addItem= document.getElementsByClassName('title')
 console.log(addItem)
@@ -21,3 +25,12 @@ addItem[0].style.fontWeight='bold'
 addItem[1].style.color='blue'
 
 
+let item=document.getElementsByClassName("list-group-item")
+console.log(item)
+item[2].style.backgroundColor='green'
+for(var items of item){
+    items.style.fontWeight='bold'
+}
+// for(var i=0;i<item.length;i++){
+//     item[i].style.fontWeight='bold'
+// }
